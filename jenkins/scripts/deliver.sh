bc5e350da779e5e93bf40dac42447221476ad846
Walter Yan <1158395969@qq.com>
2019-02-22 11:07:02 +0800

    git update-index --chmod + x

--- jenkins/scripts/deliver.sh
+++ jenkins/scripts/deliver.sh
@@ -2,6 +2,8 @@
 
 echo "Hi Jenkins"
 
+docker image ls
+
 #echo 'The following Maven command installs your Maven-built Java application'
 #echo 'into the local Maven repository, which will ultimately be stored in'
 #echo 'Jenkins''s local Maven repository (and the "maven-repository" Docker data'
